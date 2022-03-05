518c6255a4ded28ede65457903cbfdf4ff1d55a6
Made By IToncek <47974277+MadeByIToncek@users.noreply.github.com>
2022-03-05 18:33:39 +0100

    Update WeirdWelprVideo.java

--- src/main/java/cf/itoncek/weirdwelprvideo/WeirdWelprVideo.java
+++ src/main/java/cf/itoncek/weirdwelprvideo/WeirdWelprVideo.java
@@ -31,7 +31,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
